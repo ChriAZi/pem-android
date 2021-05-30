e7192992e6b00f5ed5a9160a0d09216ac2cce6af
Iris <turba@cip.ifi.lmu.de>
2021-05-30 22:48:30 +0200

    added timer

--- app/src/main/java/com/example/studywithme/ui/MainActivity.java
+++ app/src/main/java/com/example/studywithme/ui/MainActivity.java
@@ -1,66 +1,112 @@
 package com.example.studywithme.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.studywithme.R;
-import com.example.studywithme.data.models.User;
-import com.example.studywithme.ui.authentication.AuthActivity;
-import com.example.studywithme.utils.Constants;
-import com.google.android.material.button.MaterialButton;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
-    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    TextView textTimer;
+    Button start;
+    EditText et_timer;
+    CountDownTimer countdownTimer;
+    int myProgress = 0;
+    int progress;
+    int endTime = 250;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initViews();
-    }
 
-    private void initViews() {
-        User user = getUserFromIntent();
-        TextView textView = findViewById(R.id.tv_username);
-        textView.setText(user.getName());
-        MaterialButton signOutButton = findViewById(R.id.bt_sign_out);
-        signOutButton.setOnClickListener(view -> {
-            firebaseAuth.signOut();
+        progressBar = findViewById(R.id.barTimer);
+        textTimer = findViewById(R.id.timer_countdown);
+        start = findViewById(R.id.start);
+        et_timer = findViewById(R.id.session_duration);
+
+        // startTimer(5);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer();
+            }
         });
     }
 
-    private User getUserFromIntent() {
-        return (User) getIntent().getSerializableExtra(Constants.USER);
-    }
 
-    @Override
-    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        if (firebaseUser == null) {
-            startAuthActivity();
+    private void startTimer() {
+        if (et_timer.getText().toString().length() > 0) {
+            myProgress = 0;
+
+            try {
+                countdownTimer.cancel();
+
+            } catch (Exception e) {
+
+            }
+
+            String timeInterval = et_timer.getText().toString();
+            progress = 1;
+            endTime = Integer.parseInt(timeInterval); // up to finish time
+
+            countdownTimer = new CountDownTimer(60 * endTime * 1000, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    setProgress(progress, endTime*60);
+                    progress = progress + 1;
+                    int seconds = (int) (millisUntilFinished / 1000) % 60;
+                    int minutes = (int) ((millisUntilFinished / (1000 * 60)) % 60);
+                    int hours = (int) ((millisUntilFinished / (1000 * 60 * 60)) % 24);
+                    String newtime = hours + ":" + minutes + ":" + seconds;
+
+                    if (newtime.equals("0:0:0")) {
+                        textTimer.setText("00:00:00");
+                    } else if ((String.valueOf(hours).length() == 1) && (String.valueOf(minutes).length() == 1) && (String.valueOf(seconds).length() == 1)) {
+                        textTimer.setText("0" + hours + ":0" + minutes + ":0" + seconds);
+                    } else if ((String.valueOf(hours).length() == 1) && (String.valueOf(minutes).length() == 1)) {
+                        textTimer.setText("0" + hours + ":0" + minutes + ":" + seconds);
+                    } else if ((String.valueOf(hours).length() == 1) && (String.valueOf(seconds).length() == 1)) {
+                        textTimer.setText("0" + hours + ":" + minutes + ":0" + seconds);
+                    } else if ((String.valueOf(minutes).length() == 1) && (String.valueOf(seconds).length() == 1)) {
+                        textTimer.setText(hours + ":0" + minutes + ":0" + seconds);
+                    } else if (String.valueOf(hours).length() == 1) {
+                        textTimer.setText("0" + hours + ":" + minutes + ":" + seconds);
+                    } else if (String.valueOf(minutes).length() == 1) {
+                        textTimer.setText(hours + ":0" + minutes + ":" + seconds);
+                    } else if (String.valueOf(seconds).length() == 1) {
+                        textTimer.setText(hours + ":" + minutes + ":0" + seconds);
+                    } else {
+                        textTimer.setText(hours + ":" + minutes + ":" + seconds);
+                    }
+                }
+
+                @Override
+                public void onFinish() {
+                    setProgress(progress, endTime*60);
+                    textTimer.setText("Finished");
+                }
+            };
+            countdownTimer.start();
+        } else {
+            Toast.makeText(getApplicationContext(), "Please enter your session time", Toast.LENGTH_LONG).show();
         }
     }
+    public void setProgress(int startTime, int endTime) {
+        progressBar.setMax(endTime);
+        progressBar.setSecondaryProgress(endTime);
+        progressBar.setProgress(startTime);
 
-    private void startAuthActivity() {
-        Intent intent = new Intent(MainActivity.this, AuthActivity.class);
-        startActivity(intent);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        firebaseAuth.addAuthStateListener(this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        firebaseAuth.removeAuthStateListener(this);
-    }
 }
