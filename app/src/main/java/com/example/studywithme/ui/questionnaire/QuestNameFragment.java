b5605689dcbbd8bf0111f0ff2046ac83b40015ed
Julia Huber <julia.huber@bizfactory.tech>
2021-06-17 23:08:45 +0200

    add name to sessionsettings

---
+++ app/src/main/java/com/example/studywithme/ui/questionnaire/QuestNameFragment.java
@@ -0,0 +1,71 @@
+package com.example.studywithme.ui.questionnaire;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.studywithme.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link QuestNameFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class QuestNameFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private EditText editQuestName;
+
+    public QuestNameFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Quest1.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static QuestNameFragment newInstance(String param1, String param2) {
+        QuestNameFragment fragment = new QuestNameFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view;
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_quest_name, container, false);
+        editQuestName = view.findViewById(R.id.editQuestName);
+        return view;
+    }
+
+}
