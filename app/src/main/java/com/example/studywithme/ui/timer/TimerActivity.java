a655707f8d15b62d4504a2a76420bbc0b9c36341
Christopher Voit <development@christophervoit.de>
2021-07-10 22:28:14 +0200

    timer logic

--- app/src/main/java/com/example/studywithme/ui/timer/TimerActivity.java
+++ app/src/main/java/com/example/studywithme/ui/timer/TimerActivity.java
@@ -83,6 +83,8 @@ public class TimerActivity extends NavigationActivity implements TimerTaskAdapte
                 } else {
                     setTimerForPartner(session);
                 }
+            } else {
+                setTimerForOwner(session, false);
             }
         });
     }
